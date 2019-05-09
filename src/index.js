5935c55f8d23acf590dadd6ae50168506196c20c
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-05-09 14:28:59 -0500

    Archivos Inciales - npm init - Creacion del proyecto

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Buscador from './pages/Buscador.js'
+
+const container = document.getElementById('app');
+
+// ReactDOM.render(__qué__, __dónde__);
+ReactDOM.render(element, container);
---
+++ src/pages/Buscador.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Buscador extends React.Component {
+  render() {
+    return <h1>Hello I´m Search Page</h1>;
+  }
+}
+
+export default Buscador;
