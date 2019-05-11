6d77a00a41cf298f20e382d477ceb6bd93d19dba
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-05-11 10:49:34 -0500

    Form Home Styles & HandleChanges

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,24 +1,72 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
+import "./css/Search.css";
 
 class Search extends React.Component {
+  state = {
+    query: {
+      search: "",
+      serie: false,
+      movie: false
+    }
+  };
+
+  handleChange = e => {
+    console.log(e.target.value);
+
+    if (e.target.value.lenght >= 3) {
+      debugger;
+      this.history.push("/SearchList");
+    }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("No se envío");
+  };
   render() {
     return (
       <React.Fragment>
         <Header />
         <main className="MovieHome">
+          <h2 className="MovieHome-title">
+            Bienvenido al Buscador de Series y Películas #1 del{" "}
+            <strong>Mercado</strong>
+          </h2>
           <section className="MovieHome-description" />
-          <form action="" className="MovieHomeForm">
-            <input type="text" placeholder="Ingresa tu búsqueda" />
-            <div className="MovieHome">
+          <form className="MovieHomeForm" onSubmit={this.handleSubmit}>
+            <input
+              className="MovieHomeForm-search"
+              type="text"
+              name="SearchQuery"
+              placeholder="Ingresa tu búsqueda"
+              onChange={this.handleChange}
+              required
+            />
+            <div className="MovieHomeForm-checkboxes">
               <label htmlFor="">
-                <input type="checkbox" /> Filtrar por Serie
+                <input
+                  type="radio"
+                  onChange={this.handleChange}
+                  name="type"
+                  value="Serie"
+                />
+                Por Serie
               </label>
               <label htmlFor="">
-                <input type="checkbox" /> Filtrar por Película
+                <input
+                  type="radio"
+                  onChange={this.handleChange}
+                  name="type"
+                  value="Movie"
+                />
+                Por Película
               </label>
             </div>
+            <button className="btn btn-primary" onSubmit={this.handleSubmit}>
+              Buscar
+            </button>
           </form>
         </main>
       </React.Fragment>
