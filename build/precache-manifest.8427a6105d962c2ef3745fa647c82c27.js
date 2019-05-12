a7357f7108e817f836db0449f3c16bd39f9b3971
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-05-11 22:18:22 -0500

    Details

---
+++ build/precache-manifest.8427a6105d962c2ef3745fa647c82c27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4ef0e9f59440f321238",
+    "url": "/ArnoldRestrepo/movies-and-series/static/js/runtime~main.d4ef0e9f.js"
+  },
+  {
+    "revision": "aa229e09617de867b96e",
+    "url": "/ArnoldRestrepo/movies-and-series/static/js/main.aa229e09.chunk.js"
+  },
+  {
+    "revision": "c45987b433f1d9a27a1e",
+    "url": "/ArnoldRestrepo/movies-and-series/static/js/2.c45987b4.chunk.js"
+  },
+  {
+    "revision": "aa229e09617de867b96e",
+    "url": "/ArnoldRestrepo/movies-and-series/static/css/main.74d55cb2.chunk.css"
+  },
+  {
+    "revision": "49e957985f05d40dd37075d82bee3725",
+    "url": "/ArnoldRestrepo/movies-and-series/index.html"
+  }
+];
