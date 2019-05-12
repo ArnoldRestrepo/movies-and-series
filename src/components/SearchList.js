b0bd3adbf16a0b878e504a2563f880560b6aa886
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-05-11 21:39:29 -0500

    Modal Component And Styles

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -1,7 +1,20 @@
 import React from "react";
 import "./css/SearchList.css";
+import Modal from "../components/Modal";
 
 class SearchListItem extends React.Component {
+  state = {
+    modalIsOpen: false
+  };
+
+  handleCloseModal = e => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  handleOpenModal = e => {
+    e.preventDefault();
+    this.setState({ modalIsOpen: true });
+  };
   render() {
     return (
       <React.Fragment>
@@ -13,9 +26,32 @@ class SearchListItem extends React.Component {
         <div className="SearchList-item-info">
           <h3>Título: {this.props.movie.Title} </h3>
           <h4>Año: {this.props.movie.Year}</h4>
-          <a href="/" className="ShowDetails">
+          <a href="/" onClick={this.handleOpenModal} className="ShowDetails">
             Ver Detalles
           </a>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onClose={this.handleCloseModal}
+          >
+            <div className="ModalContent">
+              <h3 className="ModalContent-title">
+                Titulo: {this.props.movie.Title}
+              </h3>
+              <div className="ModalContent-body">
+                <img
+                  src={this.props.movie.Poster}
+                  alt={this.props.movie.Title}
+                  className="responsive ModalContent-img"
+                />
+                <h4 className="ModalContent-subtitle">
+                  Año: {this.props.movie.Year} <br />
+                  Tipo: {this.props.movie.Type} <br />
+                  Id de Compra: {this.props.movie.imdbID}
+                </h4>
+              </div>
+              <p />
+            </div>
+          </Modal>
         </div>
       </React.Fragment>
     );
